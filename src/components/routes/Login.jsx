3b49204e974b00724488ee825abcf20ab74e3349
Juanrubenc <105254432+Juanrubenc@users.noreply.github.com>
2022-09-08 00:09:59 -0500

    Add files via upload

--- src/components/routes/Login.jsx
+++ src/components/routes/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import LogoutForm from '../login/LogoutForm'
 import LoginForm from '../login/LoginForm'
-import '../login/styles/login.css'
+import "./styles/login.css"
+
 
 
 const Login = () => {
